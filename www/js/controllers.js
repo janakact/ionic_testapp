fafae0569a529c60a7614fe9f88bc88b666441ba
Janaka Chathuranga <bjchathuranga@gmail.com>
2015-06-22 23:02:51 +0530

    Start: Added Social Share

---
+++ www/js/controllers.js
@@ -0,0 +1,93 @@
+angular.module('starter.controllers', [])
+
+        .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+
+            // With the new view caching in Ionic, Controllers are only called
+            // when they are recreated or on app start, instead of every page change.
+            // To listen for when this page is active (for example, to refresh data),
+            // listen for the $ionicView.enter event:
+            //$scope.$on('$ionicView.enter', function(e) {
+            //});
+
+            // Form data for the login modal
+            $scope.loginData = {};
+
+            // Create the login modal that we will use later
+            $ionicModal.fromTemplateUrl('templates/login.html', {
+                scope: $scope
+            }).then(function (modal) {
+                $scope.modal = modal;
+            });
+
+            // Triggered in the login modal to close it
+            $scope.closeLogin = function () {
+                $scope.modal.hide();
+            };
+
+            // Open the login modal
+            $scope.login = function () {
+                $scope.modal.show();
+            };
+
+            // Perform the login action when the user submits the login form
+            $scope.doLogin = function () {
+                console.log('Doing login', $scope.loginData);
+
+                // Simulate a login delay. Remove this and replace with your login
+                // code if using a login system
+                $timeout(function () {
+                    $scope.closeLogin();
+                }, 1000);
+            };
+        })
+
+        .controller('PlaylistsCtrl', function ($scope) {
+            $scope.playlists = [
+                {title: 'Reggae', id: 1},
+                {title: 'Chill', id: 2},
+                {title: 'Dubstep', id: 3},
+                {title: 'Indie', id: 4},
+                {title: 'Rap', id: 5},
+                {title: 'Cowbell', id: 6}
+            ];
+        })
+
+        .controller('socialShareController', function ($scope, $cordovaCamera, $cordovaSocialSharing) {
+            var imagePath = 'img/200x200.gif';
+            var link = 'www.google.com';
+            $scope.imageURL = imagePath;
+
+            $scope.takePicture = function () {
+                var options = {
+                    quality: 50,
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    sourceType: Camera.PictureSourceType.CAMERA,
+                    allowEdit: true,
+                    encodingType: Camera.EncodingType.JPEG,
+                    targetWidth: 200,
+                    targetHeight: 200,
+                    popoverOptions: CameraPopoverOptions,
+                    saveToPhotoAlbum: false
+                };
+
+                $cordovaCamera.getPicture(options).then(function (imageData) {
+                    $scope.imageURL = "data:image/jpeg;base64," + imageData;
+                }, function (err) {
+                    console.log(err);
+                });
+            }
+
+            $scope.shareAnywhere = function () {
+                $cordovaSocialSharing
+                        .share($scope.message, $scope.subject, $scope.imageURL, link) // Share via native share sheet
+                        .then(function (result) {
+                            
+                        }, function (err) {
+                            console.log(err);
+                        });
+            }
+
+        })
+
+        .controller('PlaylistCtrl', function ($scope, $stateParams) {
+        });
